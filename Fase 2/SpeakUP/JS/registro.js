a0fa1c69c3be78339a40cebdabd718fc5a949548
Marco0T <107000167+Marco0T@users.noreply.github.com>
2026-09-11 13:20:21 -0300

    Delete Fase 2/SpeakUP directory

--- Fase 2/SpeakUP/JS/registro.js
+++
@@ -1,9 +0,0 @@
-let botonInicio = document.getElementById("registrar");
-
-botonInicio.onclick = function() {
-
-    console.log("Botón funcionando");
-
-    window.location.href = "pag_voz.html";
-
-};
