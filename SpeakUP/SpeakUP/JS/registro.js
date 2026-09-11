4ede6552214b4735fb82f13ab46939db699281da
Marco0T <107000167+Marco0T@users.noreply.github.com>
2026-09-11 13:20:02 -0300

    V2
    Agredado: menu principal, menu de inicio de sesión y registro.

---
+++ SpeakUP/SpeakUP/JS/registro.js
@@ -0,0 +1,9 @@
+let botonInicio = document.getElementById("registrar");
+
+botonInicio.onclick = function() {
+
+    console.log("Botón funcionando");
+
+    window.location.href = "pag_voz.html";
+
+};
